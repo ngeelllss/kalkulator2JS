112f866a37e1c1358ea805848e894f30524753e1
ngeelllss <angelpaquitabawondes2017@gmail.com>
2024-03-21 09:41:36 +0700

    new file:   kalkulator.css new file:   kalkulator.html new file:   kalkulator.js

---
+++ kalkulator.js
@@ -0,0 +1,51 @@
+// Function that display value
+function dis(val) {
+  document.getElementById('result').value += val;
+}
+
+function myFunction(event) {
+  if (
+    event.key == '0' ||
+    event.key == '1' ||
+    event.key == '2' ||
+    event.key == '3' ||
+    event.key == '4' ||
+    event.key == '5' ||
+    event.key == '6' ||
+    event.key == '7' ||
+    event.key == '8' ||
+    event.key == '9' ||
+    event.key == '+' ||
+    event.key == '-' ||
+    event.key == '*' ||
+    event.key == '/'
+  )
+    document.getElementById('result').value += event.key;
+}
+
+var cal = document.getElementById('calcu');
+cal.onkeyup = function (event) {
+  if (event.keyCode === 13) {
+    console.log('Enter');
+    let x = document.getElementById('result').value;
+    console.log(x);
+    solve();
+  }
+};
+
+// Function that evaluates the digit and return result
+function solve() {
+  let expression = document.getElementById('result').value;
+  try {
+    let result = eval(expression);
+    document.getElementById('result').value = result;
+  } catch (error) {
+    document.getElementById('result').value = 'Error';
+    console.error(error); // Log error to console for debugging
+  }
+}
+
+// Function that clear the display
+function clr() {
+  document.getElementById('result').value = '';
+}
